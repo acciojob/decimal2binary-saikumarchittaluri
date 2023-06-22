04cbc5b3a6e41fe58ef3517d88c8c75e25526999
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-06-22 17:18:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
- function calculateMinimumCost(n, arr) {
-  // Sort the array of rope lengths in ascending order
-  arr.sort((a, b) => a - b);
-
-  var totalCost = 0;
-
-  // Combine the two smallest ropes until only one rope is left
-  while (n > 1) {
-    // Combine the two smallest ropes
-    var smallestRope1 = arr[0];
-    var smallestRope2 = arr[1];
-    var combinedLength = smallestRope1 + smallestRope2;
-
-    // Update the total cost
-    totalCost += combinedLength;
-
-    // Remove the two smallest ropes from the array
-    arr.splice(0, 2);
-
-    // Insert the combined rope back into the array
-    arr.push(combinedLength);
-
-    // Decrease the count of remaining ropes
-    n--;
+ function decimalToBinary(decimal) {
+  let binary = "";
+  if (decimal === 0) {
+    binary = "0";
+  } else {
+    while (decimal > 0) {
+      binary = (decimal % 2) + binary;
+      decimal = Math.floor(decimal / 2);
+    }
   }
-
-  return totalCost;
+  return binary;
 }
 
-// Example usage
-var n = 5;
-var arr = [4, 2, 7, 6, 9];
-var minimumCost = calculateMinimumCost(n, arr);
-console.log(minimumCost);  // Output: 62
-
-
-module.exports = threeSum;
+// Test the function
+const decimal = parseInt(prompt("Enter a decimal number: "));
+const binary = decimalToBinary(decimal);
+console.log("Binary representation:", binary);

e98833f4cce8a876494ba9933a89e2970dc0e254
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-08-03 22:05:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function decimalToBinary(num) {
-  // Write your code here
+  if (num === 0) {
+    return "0";
+  }
+
+  let binary = "";
+  while (num > 0) {
+    binary = (num % 2) + binary;
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
